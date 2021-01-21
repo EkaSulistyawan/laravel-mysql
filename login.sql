d33cc7372618df4a013b3e82d20039c78f02a4df
kaeka <40655231+EkaSulistyawan@users.noreply.github.com>
2021-01-21 16:01:57 +0800

    Create login.sql

---
+++ login.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `admin_table` (
+   `user` varchar(10) NOT NULL,
+   `password` varchar(32) DEFAULT NULL,
+   PRIMARY KEY (`user`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
